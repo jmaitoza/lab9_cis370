87097ef97c312a0d0caa44a5f3ced3218d9d9f45
Joshua Maitoza <jmaitoza@umassd.edu>
2022-03-29 17:24:46 -0400

    finished part 4

---
+++ P4.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 128
+
+int main (int argc, char *argv[])
+{
+  char usrInput;       
+  int nread;
+  //char silly_message[SIZE] = "Nights spent maidenless ";
+  char outBuffer[SIZE];
+  int fd;
+  int count = 1;
+
+  fd = open(argv[1], O_WRONLY | O_TRUNC | O_CREAT, 0644); // O_TRUNC clears contents of file if it is not empty
+
+  
+  int stdio_var = dup(1); // checks whats currently in stdout
+
+
+  //Waiting for user input
+  while (1) {
+    scanf(" %c", &usrInput); // take usr input
+
+    if (usrInput == 'f' || usrInput == 'F') { 
+      dup2(fd,1); // changes fd to point to outbuffer 
+    }
+
+    if (usrInput == 's' | usrInput == 'S') {
+      dup2(stdio_var, 1); // changes stdout to point to outbuffer
+    }
+
+    if (usrInput == '0') {
+      dup2(stdio_var, 1);
+      printf("Exiting...");
+      break;
+    }
+
+    sprintf(outBuffer, "Nights spend maidenless %d\n", count);
+    write(1, outBuffer, strlen(outBuffer));  
+    count++;
+  }
+  close(fd);
+  return 0;
+}
---
+++ maitozaCP.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE 32
+
+int main (int argc, char *argv[])
+{
+  pid_t pid;
+  int pipeFD[2];
+  char buffer[SIZE];
+  int fd1, fd2; // file 1 input file, file 2 output file
+  int nread1, nread2;
+
+  pipe(pipeFD); // set up pipe
+
+  if (fork() == 0) { // Child Process
+    fd2 = open(argv[2], O_WRONLY); //open output file
+    if (fd2 == -1) {
+      printf("Error could not open the file\n");
+      return -1;
+    }
+    nread2 = read(pipeFD[0], buffer, SIZE); // read data from downstream end of pipe
+    write(fd2, buffer, nread2); // write to output file
+
+    close(pipeFD[0]); ///close downstream
+    exit(0);
+  }
+
+  // Parent Process
+  fd1 = open(argv[1], O_RDONLY); 
+  if (fd1 == -1) {
+    printf("Error could not open the file\n");
+    return -1;
+  }
+
+  nread1 = read(fd1, buffer, SIZE); // read input file
+  write(pipeFD[1], buffer, nread1); // write data to upstream end of pipe
+  close(pipeFD[1]); //close upstream
+  wait(NULL); //wait for child
+
+  exit(0); 
+  
+  return 0;
+}
